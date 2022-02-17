2a7329f2ba599ec417db74f2cb995011b23aa725
Bridget Robinson-Wagner <97447169+Brii4Bridget@users.noreply.github.com>
2022-02-17 10:49:06 -0600

    (7) Submitting Assignment 5

--- scriptHelper.js
+++ scriptHelper.js
@@ -51,7 +51,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
     if (fuelLevel < 10000 && cargoLevel <= 10000) {
         list.style.visibility = "visible";
-        document.getElementById ("fuelStatus").innerHTML = "Fuel Level too low for launch";
+        document.getElementById ("fuelStatus").innerHTML = "Fuel level too low for launch";
         document.getElementById ("cargoStatus").innerHTML = "Cargo mass low enough for launch";  
         document.getElementById ("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
         document.getElementById ("launchStatus").style.color = 'rgb(199, 37, 78)';
@@ -64,6 +64,15 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         document.getElementById ("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
         document.getElementById ("launchStatus").style.color = 'rgb(199, 37, 78)';
     } 
+    
+    if (cargoLevel > 10000 && fuelLevel < 10000 ) {
+        list.style.visibility = "visible";
+        document.getElementById ("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
+        document.getElementById ("fuelStatus").innerHTML = "Fuel level too low for launch";
+        document.getElementById ("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
+        document.getElementById ("launchStatus").style.color = 'rgb(199, 37, 78)';
+    }
+    
    
     if (fuelLevel >= 10000 && cargoLevel <= 10000) {
         list.style.visibility = "visible";
